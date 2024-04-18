52620e5e00a601fd2f292ed41445e6aa8d8b02a8
Lexis Dev <lexis.ua@gmail.com>
2024-04-19 02:19:25 +0300

    LOGS

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
-
+error_reporting(E_ALL);
+ini_set('error_reporting', E_ALL);
+ini_set("display_errors", 1);
 /*
 |--------------------------------------------------------------------------
 | Checking For Bagisto's Installation
