f719f7d0b8482d1277f1679a16fe94ad57030cad
Lexis Dev <lexis.ua@gmail.com>
2024-07-07 12:27:58 +0300

    B-4: New Design Template
    - Implementation of NaviStyle Design Template

--- resources/themes/navi/views/layouts/master.blade(backup).php
+++ resources/themes/navi/views/layouts/master.blade(backup).php
@@ -30,6 +30,8 @@
         <![endif]-->
 
         <!-- Document Title -->
+        <title>Home v5 | Uomo eCommerce HTML5 Template</title>
+
         {!! view_render_event('bagisto.shop.layout.head') !!}
 
         {{-- for extra head data --}}
@@ -1551,7 +1553,7 @@
 
     <div class="footer-bottom">
       <div class="container d-md-flex align-items-center">
-        <span class="footer-copyright me-auto">©2024 Navistyle.shop</span>
+        <span class="footer-copyright me-auto">©2023 Uomo</span>
         <div class="footer-settings d-md-flex align-items-center">
           <select id="footerSettingsLanguage" class="form-select form-select-sm bg-transparent border-0" aria-label="Default select example" name="store-language">
             <option class="footer-select__option" selected>United Kingdom | English</option>
@@ -2185,6 +2187,93 @@
   <div class="page-overlay"></div><!-- /.page-overlay -->
 
 <!-- TEMPLATE -->
+
+
+
+
+        <div id="app">
+            <product-quick-view v-if="$root.quickView"></product-quick-view>
+
+            <div class="main-container-wrapper">
+
+                @section('body-header')
+                    {{-- top nav which contains currency, locale and login header --}}
+                    @include('shop::layouts.top-nav.index')
+
+                    {!! view_render_event('bagisto.shop.layout.header.before') !!}
+
+                        {{-- primary header after top nav --}}
+                        @include('shop::layouts.header.index')
+
+                    {!! view_render_event('bagisto.shop.layout.header.after') !!}
+
+                    <div class="main-content-wrapper col-12 no-padding">
+
+                        {{-- secondary header --}}
+                        <header class="row velocity-divide-page vc-header header-shadow active">
+
+                            {{-- mobile header --}}
+                            <div class="vc-small-screen container">
+                                @include('shop::layouts.header.mobile')
+                            </div>
+
+                            {{-- desktop header --}}
+                            @include('shop::layouts.header.desktop')
+
+                        </header>
+
+                        <div class="">
+                            <div class="row col-12 remove-padding-margin">
+                                <sidebar-component
+                                    main-sidebar=true
+                                    id="sidebar-level-0"
+                                    url="{{ url()->to('/') }}"
+                                    category-count="{{ $velocityMetaData ? $velocityMetaData->sidebar_category_count : 10 }}"
+                                    add-class="category-list-container pt10">
+                                </sidebar-component>
+
+                                <div class="col-12 no-padding content" id="home-right-bar-container">
+                                    <div class="container-right row no-margin col-12 no-padding">
+                                        {!! view_render_event('bagisto.shop.layout.content.before') !!}
+
+                                            @yield('content-wrapper')
+
+                                        {!! view_render_event('bagisto.shop.layout.content.after') !!}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @show
+
+                <div class="container">
+                    {!! view_render_event('bagisto.shop.layout.full-content.before') !!}
+
+                        @yield('full-content-wrapper')
+
+                    {!! view_render_event('bagisto.shop.layout.full-content.after') !!}
+                </div>
+            </div>
+
+            {{-- overlay loader --}}
+            <velocity-overlay-loader></velocity-overlay-loader>
+
+            <go-top bg-color="#26A37C"></go-top>
+        </div>
+
+        {{-- footer --}}
+        @section('footer')
+            {!! view_render_event('bagisto.shop.layout.footer.before') !!}
+
+                @include('shop::layouts.footer.index')
+
+            {!! view_render_event('bagisto.shop.layout.footer.after') !!}
+        @show
+
+        {!! view_render_event('bagisto.shop.layout.body.after') !!}
+
+        {{-- alert container --}}
+        <div id="alert-container"></div>
 
         {{-- all scripts --}}
         @include('shop::layouts.scripts')
